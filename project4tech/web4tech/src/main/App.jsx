fee48768aecdf285a06d7e976c1e7f5007b192db
Anderson Lima <anderson.rl.tr@gmail.com>
2018-07-20 23:15:17 -0300

    criando a page

--- project4tech/web4tech/src/main/App.jsx
+++ project4tech/web4tech/src/main/App.jsx
@@ -1,129 +1,138 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+
+import '../css/index-style.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import logo from '../img/logo-h-vjobs.png'
+import design from '../img/designer.png'
+import testes from '../img/tester.png'
+import developer from '../img/developer.png'
 
 class App extends Component {
   render() {
     return (
       <div>
-        <nav class="navbar navbar-inverse">
-            <ul class="nav nav-pills">
+        <nav className="navbar navbar-inverse">
+            <ul className="nav nav-pills">
                 <li>
-                    <a href="#"> <img src="img/logo-h-vjobs.png" class="logo" /> </a>
+                    <a href=""> <img src={logo} alt="logo" className="logo" /> </a>
                 </li>
-                <li class="style-lis">
-                    <a href="#"> Vagas </a>
+                <li className="style-lis">
+                    <a href=""> Vagas </a>
                 </li>
-                <li class="style-lis">
-                    <a href="#"> Sobre </a>
+                <li className="style-lis">
+                    <a href=""> Sobre </a>
                 </li>
             </ul>
         </nav>
 
-        <div class="container">
+        <div className="container">
 
             <p>
-                <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                <button className="btn btn-primary" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
                     Nova Vaga
                 </button>
             </p>
 
-            <div class="collapse" id="collapseExample">
-                <div class="card card-body">
-                    <form class="form-body">
-                        <div class="form-group">
-                            <label for="nome">Nome</label><br/>
-                            <input id="nome" type="text" class="form-control" />
+            <div className="collapse" id="collapseExample">
+                <div className="card card-body">
+                    <form className="form-body">
+                        <div className="form-group">
+                            <label htmlFor="nome">Nome</label><br/>
+                            <input id="nome" type="text" className="form-control" />
                         </div>
 
-                        <div class="form-group">
-                            <label for="nome">Descriçao</label><br/>
-                            <textarea rows="4" cols="50" class="form-control">
+                        <div className="form-group">
+                            <label htmlFor="nome">Descriçao</label><br/>
+                            <textarea rows="4" cols="50" className="form-control">
                             </textarea>
                         </div>
 
-                        <div class="row form-group">
-                            <div class="col-lg-6 col-md-6 col-sm-12 mb-15">
-                                <label for="nome">Habilidades Necessárias</label><br/>
-                                <textarea rows="4" cols="50" class="form-control "></textarea>
+                        <div className="row form-group">
+                            <div className="col-lg-6 col-md-6 col-sm-12 mb-15">
+                                <label htmlFor="nome">Habilidades Necessárias</label><br/>
+                                <textarea rows="4" cols="50" className="form-control "></textarea>
                             </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <label for="nome">Diferenciais</label><br/>
-                                <textarea rows="4" cols="50" class="form-control"></textarea>
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <label htmlFor="nome">Diferenciais</label><br/>
+                                <textarea rows="4" cols="50" className="form-control"></textarea>
                             </div>
                         </div>
 
-                        <div class="row form-group">
-                            <div class="col-lg-6 col-md-6 col-sm-12 mb-15">
-                                <label for="salarioBase">Salario base</label><br/>
-                                <input id="salarioBase" type="text" class="form-control " />
+                        <div className="row form-group">
+                            <div className="col-lg-6 col-md-6 col-sm-12 mb-15">
+                                <label htmlFor="salarioBase">Salario base</label><br/>
+                                <input id="salarioBase" type="text" className="form-control " />
                             </div>
-                            <div class="col-lg-6 col-md-6 col-sm-12">
-                                <label for="nome">Área</label><br/>
-                                <select class="form-control">
+                            <div className="col-lg-6 col-md-6 col-sm-12">
+                                <label htmlFor="nome">Área</label><br/>
+                                <select className="form-control">
                                     <option>Desenvolvimento</option>
                                 </select>
                             </div>
                         </div>
-                        <div class="form-group">
+                        <div className="form-group">
                             <input type="checkbox" /> Vaga para PCD
                         </div>
-                        <div class="form-group text-right">
-                            <input type="submit" class="btn btn-success" value="Criar vaga" />
+                        <div className="form-group text-right">
+                            <input type="submit" className="btn btn-success" value="Criar vaga" />
                         </div>
                     </form>
                 </div>
             </div>
 
-            <div class="row">
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xm-12">
-                    <div class="bloco">
-                        <img src="img/designer.png" class="img-responsive" />
+            <div className="row">
+                <div className="col-lg-4 col-md-6 col-sm-6 col-xm-12">
+                    <div className="bloco">
+                        <img src={design} alt="design" className="img-responsive" />
                         <h3>Teste</h3>
                         <b>Descriçao:</b>
                         <p>Algumas coisa</p>
                         <b>Salario base:</b>
                         <p>R$: 10,00</p>
                         <p>
-                            <button class="btn-warning">
-                                <span class="fa fa-edit icons"></span>
+                            <button className="btn-warning">
+                                <span className="fa fa-edit icons"></span>
                             </button>
-                            <button class="btn-danger">
-                                <span class="fa fa-trash icons"></span>
+                            <button className="btn-danger">
+                                <span className="fa fa-trash icons"></span>
                             </button>
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xm-12">
-                    <div class="bloco">
-                        <img src="img/developer.png" class="img-responsive" />
+                <div className="col-lg-4 col-md-6 col-sm-6 col-xm-12">
+                    <div className="bloco">
+                        <img src={developer} alt="developer" className="img-responsive" />
                         <h3>Teste</h3>
                         <b>Descriçao:</b>
                         <p>Algumas coisa</p>
                         <b>Descriçao:</b>
                         <p>R$: 10,00</p>
                         <p>
-                            <button class="btn-warning">
-                                <span class="fa fa-edit icons"></span>
+                            <button className="btn-warning">
+                                <span className="fa fa-edit icons"></span>
                             </button>
-                            <button class="btn-danger">
-                                <span class="fa fa-trash icons"></span>
+                            <button className="btn-danger">
+                                <span className="fa fa-trash icons"></span>
                             </button>
                         </p>
                     </div>
                 </div>
-                <div class="col-lg-4 col-md-6 col-sm-6 col-xm-12">
-                    <div class="bloco">
-                        <img src="img/tester.png" class="img-responsive" />
+                <div className="col-lg-4 col-md-6 col-sm-6 col-xm-12">
+                    <div className="bloco">
+                        <img src={testes} alt="testes" className="img-responsive" />
                         <h3>Teste</h3>
                         <b>Descriçao:</b>
                         <p>Algumas coisa</p>
                         <b>Descriçao:</b>
                         <p>R$: 10,00</p>
                         <p>
-                            <button class="btn-warning">
-                                <span class="fa fa-edit icons"></span>
+                            <button className="btn-warning">
+                                <span className="fa fa-edit icons"></span>
                             </button>
-                            <button class="btn-danger">
-                                <span class="fa fa-trash icons"></span>
+                            <button className="btn-danger">
+                                <span className="fa fa-trash icons"></span>
                             </button>
                         </p>
                     </div>
